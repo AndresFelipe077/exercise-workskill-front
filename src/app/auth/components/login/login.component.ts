3135750d308d5509ab5f0df75f24e0ff40c96109
sena <pipicanodayana2005@gmail.com>
2023-05-19 11:30:01 -0500

    Se suben cambios de frontend

--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -3,8 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { LoginService } from 'src/app/service/login.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-
-
+import { AppComponent } from 'src/app/app.component';
 
 @Component({
   selector: 'app-login',
@@ -19,7 +18,8 @@ export class LoginComponent implements OnInit {
     private http: HttpClient,
     private login: LoginService,
     private formBuilder: FormBuilder,
-    private router: Router
+    private router: Router,
+    private appComponent:AppComponent,
   ) { }
 
   ngOnInit(): void {
@@ -27,6 +27,7 @@ export class LoginComponent implements OnInit {
       usuario: ['', Validators.required],
       password: ['', Validators.required],
     });
+    this.cerrarSesion();
   }
 
   loginUser(userData: any)
@@ -35,11 +36,17 @@ export class LoginComponent implements OnInit {
       (response: any) => {
         console.log(response);
         this.router.navigate(['/dashboard']);
+        this.dataUser.reset();
+        this.appComponent.userLoginOn = true;
       },
       (error: any) => {
         console.log(error);
       }
     );
+  }
+
+  cerrarSesion(){
+    this.appComponent.userLoginOn = false;
   }
 
 
